6fe6b547666d5a81239111f87fd475d2634cdc3b
Josep Subils <josep@spellbook.io>
2017-06-27 12:48:02 +0200

    [update] add development and production mode

--- conf.js
+++ conf.js
@@ -1,14 +1,23 @@
 module.exports = {
   http_port: 3000,
   sockets_port: 3001,
+  mode: 'development',
   database: {
-    client: 'mysql',
-    connection: {
-      host : '127.0.0.1',
-      user : 'your_database_user',
-      password : 'your_database_password',
-      database : 'myapp_test'
+    production : {
+      client: 'mysql',
+      connection: {
+        host : '127.0.0.1',
+        user : 'your_database_user',
+        password : 'your_database_password',
+        database : 'myapp_test'
+      },
+      pool: { min: 0, max: 7 }
     },
-    pool: { min: 0, max: 7 }
+    development : {
+      client: 'sqlite3',
+      connection: {
+        filename: "./mydb.sqlite"
+      }
+    }
   }
 }
--- server/db_gen.js
+++ server/db_gen.js
@@ -1,3 +1,3 @@
 const conf = require('../conf')
-const knex = require('knex')(conf)
+const knex = require('knex')(conf.database[conf.mode])
 module.exports = knex
