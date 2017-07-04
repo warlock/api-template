326d66a8e6262b50132343601dc1d2e697e01667
Josep Subils <josep@spellbook.io>
2017-07-04 15:26:45 +0200

    [update] socket database change to knex orm

--- server/socket_gen.js
+++ server/socket_gen.js
@@ -1,54 +1,62 @@
-var validator = require('./validator.js')
-
 module.exports = serv => {
   return {
     gen : (model, callback) => {
       serv.socket.on(`${model}GetAll`, (data) => {
-        serv.db[model].find({}, (err, docs) => {
-          if (err) {
-            console.log(`SOCKET getArticles ERROR: ${err}`)
-            serv.socket.emit(`${model}GetAll`, { err : err })
-          } else serv.socket.emit(`${model}GetAll`, { articles : docs })
+        serv.db.select().from(model)
+        .then(docs => {
+          res.json(docs)
+        })
+        .catch(err => {
+          console.log(`SOCKET getArticles ERROR: ${err}`)
+          res.json({ query : 'get', model : model, err : JSON.stringify(err) })
         })
       })
 
       serv.socket.on(`${model}Add`, (data) => {
-        serv.db[model].insert(validator(model, data), (err, newDoc) => {
-          if (err) {
-            console.log(`SOCKET ${model}Add ERROR: ${err}`)
-            serv.socket.emit(`${model}Add`, { err : err })
-          } else serv.socket.emit(`${model}Add`, { res : true })
+        serv.db(model).insert(req.body)
+        .then(docs => {
+          res.json({ res : true, total : docs })
+        })
+        .catch(err => {
+          console.log(`SOCKET ${model}Add ERROR: ${err}`)
+          res.json({ query : 'post', model : model, err : JSON.stringify(err) })
         })
       })
 
       serv.socket.on(`${model}Get`, (data) => {
-        serv.db[model].find({ _id : data.article_id }, (err, docs) => {
-          if (err) {
-            console.log(`SOCKET ${model}Get ERROR: ${err}`)
-            serv.socket.emit(`${model}Get`, { err : err })
-          } else serv.socket.emit(`${model}Get`, { res : true })
+        serv.db.select().from(model).where({ id : req.params.id })
+        .then(docs => {
+          res.json(docs)
+        })
+        .catch(err => {
+          console.log(`SOCKET ${model}Get ERROR: ${err}`)
+          res.json({ query : 'getById', model : model, err : JSON.stringify(err) })
         })
       })
 
       serv.socket.on(`${model}Update`, (data) => {
-        serv.db[model].find({ _id : data.article_id }, (err, docs) => {
-          if (err) {
-            console.log(`SOCKET ${model}Update ERROR: ${err}`)
-            serv.socket.emit(`${model}Update`, { err : err })
-          } else serv.socket.emit(`${model}Update`, { res : true })
+        serv.db(model).where({ id : req.params.id }).update(req.body)
+        .then(docs => {
+          res.json({ res : true, total : docs })
+        })
+        .catch(err => {
+          console.log(`SOCKET ${model}Update ERROR: ${err}`)
+          res.json({ query : 'updateById', model : model, err : JSON.stringify(err) })
         })
       })
 
       serv.socket.on(`${model}Delete`, (data) => {
-        serv.db[model].remove({ _id: req.params.article_id }, {}, (err, numRemoved) => {
-          if (err) {
-            console.log(`SOCKET ${model}Delete ERROR: ${err}`)
-            serv.socket.emit(`${model}Delete`, { err : err })
-          } else serv.socket.emit(`${model}Delete`, { res : true })
+        serv.db(model).where({ id : req.params.id }).delete()
+        .then(docs => {
+          res.json({ res : true, total : docs })
+        })
+        .catch(err => {
+          console.log(`SOCKET ${model}Delete ERROR: ${err}`)
+          res.json({ query : 'deleteById', model : model, err : JSON.stringify(err) })
         })
       })
 
-      if (Object.prototype.toString.call(callback) == '[object Function]') callback(serv.socket, serv.db[model])
+      if (Object.prototype.toString.call(callback) == '[object Function]') callback(serv.socket, serv.db)
     }
   }
 }
