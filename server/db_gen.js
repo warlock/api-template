e44d38480f80ea59506028acf0635bff2a3a2477
Josep Subils <josep@spellbook.io>
2017-07-04 15:20:12 +0200

    [update] autogenerate a new database

--- server/db_gen.js
+++ server/db_gen.js
@@ -1,19 +1,20 @@
 const conf = require('../conf')
 const knex = require('knex')(conf.database[conf.mode])
-const schema = require('./schema')
+const schema = require('../schema')
 module.exports = () => {
   Object.keys(schema).forEach( model => {
-    knex.schema.createTable(model, table => {
-      table.increments('uid').primary()
+    knex.schema.createTableIfNotExists(model, table => {
+      table.increments('id').primary()
       table.timestamps()
       Object.keys(schema[model]).forEach(value => {
         table[schema[model][value]](value)
       })
+    }).then(() => {
+      console.log(`Table created: ${model}`)
     })
-  }).then(() => {
-    console.log("Tablas creadas")
+    .catch(err => {
+      console.error(err)
+    })
   })
-  .catch(err => {
-    console.log(err)
-  })
+  return knex
 }
