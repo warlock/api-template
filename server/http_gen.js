c2ef79dd9cb992c9babeb71571bef55da5f25b28
Josep Subils <josep@spellbook.io>
2017-07-04 15:20:37 +0200

    [update] change nedb to knex

--- server/http_gen.js
+++ server/http_gen.js
@@ -1,48 +1,61 @@
-var validator = require('./validator.js')
-
 module.exports = serv => {
   return {
     gen : (model, callback) => {
       serv.http.route(`/${model}`)
         .post((req, res) => {
           console.log(`${model} POST!`)
-          serv.db[model].insert(validator(model, req.body), (err, newDoc) => {
-            if (err) res.json({ res : false, err : `Have a problem inserting in db ${err}!` })
-            else res.json({ res : true })
+          serv.db(model).insert(req.body)
+          .then(docs => {
+            res.json({ res : true, total : docs })
+          })
+          .catch(err => {
+            res.json({ query : 'post', model : model, err : JSON.stringify(err) })
           })
         })
         .get((req, res) => {
           console.log(`${model} GET ALL!`)
-          serv.db[model].find({}, (err, docs) => {
-            if (err) console.log(err)
-            else res.json(docs)
+          serv.db.select().from(model)
+          .then(docs => {
+            res.json(docs)
+          })
+          .catch(err => {
+            res.json({ query : 'get', model : model, err : JSON.stringify(err) })
           })
         })
 
       serv.http.route(`/${model}/:id`)
         .get((req, res) => {
           console.log(`${model} GET: ${req.params.id}`)
-          serv.db[model].find({ _id : req.params.id }, (err, docs) => {
-            if (err) console.log(err)
-            else res.json(docs[0])
+          serv.db.select().from(model).where({ id : req.params.id })
+          .then(docs => {
+            res.json(docs)
+          })
+          .catch(err => {
+            res.json({ query : 'getById', model : model, err : JSON.stringify(err) })
           })
         })
         .put((req, res) => {
           console.log(`${model} PUT: ${req.params.id}`)
-          serv.db[model].update({ _id: req.params.id }, validator(model, req.body), (err, numReplaced) => {
-            if (err) res.json({ res : false, err : err })
-            else res.json({ res : true, numReplaced : numReplaced })
+          serv.db(model).where({ id : req.params.id }).update(req.body)
+          .then(docs => {
+            res.json({ res : true, total : docs })
+          })
+          .catch(err => {
+            res.json({ query : 'updateById', model : model, err : JSON.stringify(err) })
           })
         })
         .delete((req, res) => {
           console.log(`${model} DELETE: ${req.params.id}`)
-          serv.db[model].remove({ _id: req.params.id }, {}, (err, numRemoved) => {
-            if (err) res.json({ res : false, err : err })
-            else res.json({ res : true, numRemoved : numRemoved })
+          serv.db(model).where({ id : req.params.id }).delete()
+          .then(docs => {
+            res.json({ res : true, total : docs })
+          })
+          .catch(err => {
+            res.json({ query : 'deleteById', model : model, err : JSON.stringify(err) })
           })
         })
 
-      if (Object.prototype.toString.call(callback) == '[object Function]') callback(serv.http, serv.db[model])
+      if (Object.prototype.toString.call(callback) == '[object Function]') callback(serv.http, serv.db)
     }
   }
 }
