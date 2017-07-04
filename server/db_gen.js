f1b32d234f6bd15e5d40ad52266f5a432e75d5c5
Josep Subils <josep@spellbook.io>
2017-07-04 14:51:53 +0200

    [update] generate schemas with knex

--- server/db_gen.js
+++ server/db_gen.js
@@ -1,3 +1,19 @@
 const conf = require('../conf')
 const knex = require('knex')(conf.database[conf.mode])
-module.exports = knex
+const schema = require('./schema')
+module.exports = () => {
+  Object.keys(schema).forEach( model => {
+    knex.schema.createTable(model, table => {
+      table.increments('uid').primary()
+      table.timestamps()
+      Object.keys(schema[model]).forEach(value => {
+        table[schema[model][value]](value)
+      })
+    })
+  }).then(() => {
+    console.log("Tablas creadas")
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}
